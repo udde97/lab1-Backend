cf7025619514304dfdffe6277744d02ddb79a75c
mahamudabukar <maab970514@student.jenseneducation.se>
2021-12-16 15:46:32 +0100

    screenshot

---
+++ lab1/DTO/AnimalDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using lab1.Entity;
+
+namespace lab1.DTO
+{
+    public class AnimalDTO
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+
+}
